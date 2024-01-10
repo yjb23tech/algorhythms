ed93d3cbf464a207ef83fae240a7dba0a5f0c91c
yjb23tech <yjb23tech@gmail.com>
2024-01-10 19:25:02 +0000

    Pepper Pots

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -2,7 +2,7 @@ const NavigationBar = (props) => {
     return (
         <nav className="nav-bar">
             <div className="nav-bar-logo-home nav-bar-fonts">
-                <h1>{props.website_title}</h1>
+                <h3>{props.website_title}</h3>
             </div>
             <div className="nav-bar-page-options nav-bar-fonts util-box-border">
                 <p>{props.name_page_one}</p>
