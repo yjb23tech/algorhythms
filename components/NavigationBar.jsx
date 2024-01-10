81114b4a5147fca2a159c757e8de21c2108d0b72
yjb23tech <yjb23tech@gmail.com>
2024-01-10 18:54:23 +0000

    Jarvis

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -1,10 +1,10 @@
 const NavigationBar = (props) => {
     return (
-        <nav>
-            <div className="nav-bar-logo-home">
+        <nav className="nav-bar">
+            <div className="nav-bar-logo-home nav-bar-fonts">
                 <h1>{props.website_title}</h1>
             </div>
-            <div className="nav-bar-page-options">
+            <div className="nav-bar-page-options nav-bar-fonts">
                 <p>{props.name_page_one}</p>
                 <p>{props.name_page_two}</p>
                 <p>{props.name_page_three}</p>
