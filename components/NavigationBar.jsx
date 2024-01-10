6fe8433422ca0f2906e2681c0ebf8ce4a32cadb2
yjb23tech <yjb23tech@gmail.com>
2024-01-10 19:12:52 +0000

    Obadiah IronMonger

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -4,7 +4,7 @@ const NavigationBar = (props) => {
             <div className="nav-bar-logo-home nav-bar-fonts">
                 <h1>{props.website_title}</h1>
             </div>
-            <div className="nav-bar-page-options nav-bar-fonts">
+            <div className="nav-bar-page-options nav-bar-fonts util-box-border">
                 <p>{props.name_page_one}</p>
                 <p>{props.name_page_two}</p>
                 <p>{props.name_page_three}</p>
