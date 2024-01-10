e4e8c4d864642feba29b9a86e66d64123cd81349
yjb23tech <yjb23tech@gmail.com>
2024-01-10 19:53:14 +0000

    Shameless

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 const NavigationBar = (props) => {
     return (
         <nav className="nav-bar">
@@ -5,8 +6,12 @@ const NavigationBar = (props) => {
                 <h3>{props.website_title}</h3>
             </div>
             <div className="nav-bar-page-options nav-bar-fonts util-box-border">
-                <p>{props.name_page_one}</p>
-                <p>{props.name_page_two}</p>
+                <Link to={`/`} className="link-to-dashboard">
+                    <p>{props.name_page_one}</p>
+                </Link>
+                <Link to={`/explorer`} className="link-to-explorer">
+                    <p>{props.name_page_two}</p>
+                </Link>
                 <p>{props.name_page_three}</p>
             </div>
         </nav>
